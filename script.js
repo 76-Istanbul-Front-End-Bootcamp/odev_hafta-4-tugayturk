62b79839c365c70eaebc58e3f80e52ac29fdff0e
Tugay Türk <i.tugayturk@gmail.com>
2020-12-24 18:44:21 +0000

    ödev çözümü

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const data = {
-  USD: {EUR: 0.82, GBP: 0.74},
-  EUR: {USD: 1.23, GBP: 0.91},
-  GBP: {USD: 1.35, EUR: 1.10},
+  USD: { EUR: 0.82, GBP: 0.74, TRY: 7.64 },
+  EUR: { USD: 1.23, GBP: 0.91, TRY: 9.36 },
+  GBP: { USD: 1.35, EUR: 1.10, TRY: 10.28 },
+  TRY: { USD: 0.13, EUR: 0.11, GBP: 0.097 }
 };
 
 const currencyKeys = Object.keys(data);
 
-function createCurrencyElements(elements, root, inputName){
-  for(let i =0; i< elements.length; i++){
-    const currencyKeyDiv   = document.createElement("div");
+function createCurrencyElements(elements, root, inputName) {
+  for (let i = 0; i < elements.length; i++) {
+    const currencyKeyDiv = document.createElement("div");
     const currencyKeyInput = document.createElement("input");
     currencyKeyInput.setAttribute("type", "radio");
     currencyKeyInput.setAttribute("name", inputName);
@@ -37,18 +38,43 @@ createCurrencyElements(currencyKeys, parentToEl, toInputName);
 
 
 const calculateButton = document.querySelector("#calculate-button");
-calculateButton.addEventListener("click", function(){
-   // kimden ceviriyourz
-   const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
-   // kime ceviriyoruz
-   const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
-   // amountu alalim
-   const amount     = document.querySelector("input[name='amount']").value;
 
-   const currentCurrencyObject = data[fromTarget];
-   const resultForOne = currentCurrencyObject[toTarget];
-   const result = amount * resultForOne;
+calculateButton.addEventListener("click", function () {
+  const currencyResult = document.querySelector("#currency-result");
+  if (document.querySelector("input[name='currency_to']:checked") === null || document.querySelector("input[name='currency_from']:checked") === null) {
+    myFunction("Please Make A Choice!")
 
-   const currencyResult = document.querySelector("#currency-result");
-   currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
-});
+  }
+  else {
+    const amount = document.querySelector("input[name='amount']").value;
+    if (document.querySelector("input[name='currency_to']:checked").value === document.querySelector("input[name='currency_from']:checked").value) {
+      myFunction("Please Make Different Choices!")
+    } else if (isNaN(amount)) {
+      myFunction("The Entry Must Be a Number! ");
+
+    }
+    else {
+      const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
+      const toTarget = document.querySelector("input[name='currency_to']:checked").value;
+
+      const currentCurrencyObject = data[fromTarget];
+      const resultForOne = currentCurrencyObject[toTarget];
+      const result = amount * resultForOne;
+      currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
+      return;
+    }
+
+
+  }
+
+})
+function myFunction(message) {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+  document.querySelector("#snackbar").innerHTML = message;
+}
+
+
